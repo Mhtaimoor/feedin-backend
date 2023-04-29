b09317bcbc8b411a7b6ca1911abd58c0cd8a4f05
Muhammad Taimoor <90235270+Mhtaimoor@users.noreply.github.com>
2023-04-29 23:07:48 +0500

    making app serverless-http

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+const serverless = require("serverless-http");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const env = require("dotenv");
-const router = express.Router();
 
 const userRoutes = require("./routes/users");
 const brandsRoutes = require("./routes/brands");
@@ -11,6 +11,18 @@ const brandsRoutes = require("./routes/brands");
 env.config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+// app.use("/public", express.static(path.join(__dirname, "uploads")));
+app.use("/users", userRoutes);
+app.use("/brands", brandsRoutes);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello to FeedInn API</h1>");
+});
 app.listen(
   process.env.PORT,
   console.log(`Server running on port: ${process.env.PORT}`)
@@ -24,14 +36,5 @@ mongoose.connect(
   console.log("MongoDB Connection Established")
 );
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello to FeedInn API</h1>");
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-// app.use("/public", express.static(path.join(__dirname, "uploads")));
-app.use("/users", userRoutes);
-app.use("/brands", brandsRoutes);
+// wrap the express app with serverless-http
+module.exports.handler = serverless(app);
